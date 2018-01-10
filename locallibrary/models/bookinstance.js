50f3ff36b01e7c7e990fbc2126e0d14b63cdef22
Gabe Larkin <larkin330@gmail.com>
2018-01-10 00:08:50 +0000

    bathroom break

--- locallibrary/models/bookinstance.js
+++ locallibrary/models/bookinstance.js
@@ -1,1 +1,20 @@
 // bookinstance.js
+const   mongoose = require("mongoose"),
+        Schema = mongoose.Schema;
+        
+var BookInstanceSchema = new Schema({
+    book: { type: Schema.ObjectId, ref: 'Book', required: true }, // ref to the associated book
+    imprint: { type: String, required: true },
+    status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance' },
+    due_back: { type: Date, default: Date.now },
+});
+
+// virtual for BookInstance's URL
+BookInstanceSchema
+    .virtual('url')
+    .get( () => {
+        return '/catalog/bookinstance/' + this._id
+    });
+    
+// compile and export
+module.exports = mongoose.model('Book Instance',    BookInstanceSchema);
--- locallibrary/models/genre.js
+++ locallibrary/models/genre.js
@@ -1,1 +1,15 @@
 // genre.js
+const   mongoose = require("mongoose"),
+        Schema = mongoose.Schema;
+    
+var GenreSchema = new Schema({
+    name: { type: String, required: true, min: 3, max: 100 },
+});
+
+GenreSchema
+    .virtual('url')
+    .get( () => {
+        return '/catalog/genre/' + this._id;
+    });
+    
+module.exports = mongoose.model('Genre', GenreSchema);
